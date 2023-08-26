f73a0383fcf3d2282902fe7678031743a4194e63
Cristina Hurtado <churtado@correo.unimet.edu.ve>
2023-08-26 19:41:56 -0400

    sidebar started

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./Sidebar.modules.css";
+
+export default function Sidebar() {
+    
+  return (
+    <div className={styles.container}>
+      <ul>
+        <li>
+          <div className={styles.btn_div}>
+            <button className={styles.profile}>Método de Cálculo</button>
+          </div>
+        </li>
+        <hr />
+        <li>
+          <div className={styles.btn_div}>
+            <button className={styles.profile}>Perfil del suelo</button>
+          </div>
+        </li>
+        <hr />
+        <li>
+          <div className={styles.btn_div}>
+            <button className={styles.profile}>Dimensiones del pilote</button>
+          </div>
+        </li>
+        <hr />
+        <li>
+          <div className={styles.btn_div}>
+            <button className={styles.profile}>Perfil</button>
+          </div>
+        </li>
+        <hr />
+      </ul>
+    </div>
+  );
+}
--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./App.modules.css";
 import Navbar from "../Components/Navbar/Navbar";
+import Sidebar from "../Components/Sidebar/Sidebar";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Sidebar/>
     </>
   );
 }
