4cfb53b333d1c6255bd0269ae0fe4de3c29749f8
Cristina Hurtado <churtado@correo.unimet.edu.ve>
2023-09-23 18:40:22 -0400

    soil layers component dynamic

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import Select from "react-select";
 import styles from "./Profile.modules.css";
-import SoilLayer from "./SoilLayer";
 
 export default function Profile({ callback }) {
+  const soilTypes = [
+    {
+      value: "1",
+      label: "Relleno (Fin)",
+    },
+    {
+      value: "2",
+      label: "Arena (Sand)",
+    },
+    {
+      value: "3",
+      label: "Grava (Gravel)",
+    },
+    {
+      value: "4",
+      label: "Arcilla (Clay)",
+    },
+    {
+      value: "5",
+      label: "Limo (Mud)",
+    },
+    {
+      value: "6",
+      label: "Arcilla de alta plasticidad",
+    },
+  ];
+  const [soilList, setSoilList] = useState([
+    { type: "", start: "", end: "", ngp: "" },
+  ]);
+  const [selectedValue, setSelectedValue] = useState(0); //Almacena valor del dropdown
+
+  const [changedValue, setChangedValue] = useState(false);
+
+  console.log(soilList);
+
+  const handleLayerAdd = () => {
+    setSoilList([...soilList, { type: "", start: "", end: "", ngp: "" }]);
+  };
+
+  const handleLayerRemove = (index) => {
+    const list = [...soilList];
+    list.splice(index, 1);
+    setSoilList(list);
+  };
+
+  const handleStartChange = (e, index) => {
+    console.log(index);
+    const { value } = e.target;
+    const list = [...soilList];
+    list[index]["start"] = value;
+    if (changedValue) {
+      list[index]["type"] = selectedValue;
+      setSoilList(list);
+      setChangedValue(false);
+    }else{
+      
+      setSoilList(list);
+    }
+  };
+
+  const handleEndChange = (e, index) => {
+    const { value } = e.target;
+    const list = [...soilList];
+    list[index]["end"] = value;
+    if (changedValue) {
+      list[index]["type"] = selectedValue;
+      setSoilList(list);
+      setChangedValue(false);
+    }else{
+      
+      setSoilList(list);
+    }
+  };
+
+  const handleNgpChange = (e, index) => {
+    const { value } = e.target;
+    const list = [...soilList];
+    list[index]["ngp"] = value;
+    if (changedValue) {
+      list[index]["type"] = selectedValue;
+      setSoilList(list);
+      setChangedValue(false);
+    }else{
+      
+      setSoilList(list);
+    }
+  };
+
+  const handleSelectedValue = (Obj) => {
+    setSelectedValue(Obj.value);
+    setChangedValue(true);
+  };
+
+/*   const handleTypeChange = (index) => {
+    console.log(index);
+    const list = [...soilList];
+    list[index]["type"] = selectedValue;
+    setSoilList(list);
+    setChangedValue(false);
+  }; */
+
   const show = false;
   return (
     <div className={styles.div_container}>
@@ -21,13 +121,66 @@ export default function Profile({ callback }) {
           </li>
         </ul>
       </div>
-      <SoilLayer />
-      <SoilLayer />
-      <SoilLayer />
-      <SoilLayer />
-      <SoilLayer />
+      <div className={styles.form}>
+        {soilList.map((singleSoil, index) => (
+          <div key={index} className={styles.comp}>
+            <ul>
+              <li>
+                <Select
+                  className={styles.select_soil}
+                  placeholder="Seleccione un tipo de suelo"
+                  options={soilTypes}
+                  onChange={handleSelectedValue}
+                />
+              </li>
+              <li>
+                <div>
+                  <label>Inicio</label>
+                  <input
+                    placeholder="0"
+                    value={singleSoil.start}
+                    onChange={(e) => handleStartChange(e, index)}
+                  ></input>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <label>Fin</label>
+                  <input
+                    placeholder="0"
+                    onChange={(e) => handleEndChange(e, index)}
+                  ></input>
+                </div>
+              </li>
+              <li>
+                <div>
+                  <label>Num G/P</label>
+                  <input
+                    placeholder="20"
+                    onChange={(e) => handleNgpChange(e, index)}
+                  ></input>
+                </div>
+              </li>
+              <li>
+                {soilList.length > 1 && (
+                  <div>
+                    <button
+                      className={styles.xBtn}
+                      onClick={() => handleLayerRemove(index)}
+                    >
+                      X
+                    </button>
+                  </div>
+                )}
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
       <div className={styles.add}>
-        <button className={styles.addBtn}>Agregar otro estrato</button>
+        <button className={styles.addBtn} onClick={handleLayerAdd}>
+          Agregar otro estrato
+        </button>
       </div>
     </div>
   );
